36f77691b59a8cd31502076ca5bc5c34ae4cb940
Dhinid Warnasooriya <phoenixfire696@gmail.com>
2022-10-12 22:26:10 +0530

    link files

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>E-Shop</title>
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+
+
+
+
+
+    <script src="./script.js"></script>
+</body>
+
+</html>
