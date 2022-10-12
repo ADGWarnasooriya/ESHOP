8f22666a2b71c0c30af12321d09a2b36cc1d19c8
Dhinid Warnasooriya <phoenixfire696@gmail.com>
2022-10-13 00:16:36 +0530

    header column

--- index.php
+++ index.php
@@ -17,6 +17,12 @@
     <div class="container-fluid vh-100 d-flex justify-content-center">
         <div class="row align-content-center"></div>
 
+        <!-- header -->
+        <div class="col-12">
+
+        </div>
+        <!-- header -->
+
     </div>
 
 
