097d370dc2df352c202cd7f6ee82a4be40c44350
Dhinid Warnasooriya <phoenixfire696@gmail.com>
2022-10-15 01:15:26 +0530

    logo

--- index.php
+++ index.php
@@ -19,7 +19,9 @@
 
         <!-- header -->
         <div class="col-12">
-
+            <div class="row">
+                <div class="col-12 logo"></div>
+            </div>
         </div>
         <!-- header -->
 
