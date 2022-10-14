81cc54d91a93782e0b73d6b4997f60e4cf504e75
Dhinid Warnasooriya <phoenixfire696@gmail.com>
2022-10-15 03:52:40 +0530

    div error fixed

--- index.php
+++ index.php
@@ -15,30 +15,30 @@
 
 <body class="main-body">
     <div class="container-fluid vh-100 d-flex justify-content-center">
-        <div class="row align-content-center"></div>
+        <div class="row align-content-center">
 
-        <!-- header -->
-        <div class="col-12">
-            <div class="row">
-                <div class="col-12 logo"></div>
-                <div class="col-12">
-                    <p class="text-center title1">Hi, welcome to eshop</p>
+            <!-- header -->
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-12 logo"></div>
+                    <div class="col-12">
+                        <p class="text-center title1">Hi, welcome to eshop</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <!-- header -->
+            <!-- header -->
 
-        <!-- content -->
-        <div class="col-12 p-3">
-            <div class="row">
-                <div class="col-6 d-none d-lg-block background"></div>
+            <!-- content -->
+            <div class="col-12 p-3">
+                <div class="row">
+                    <div class="col-6 d-none d-lg-block background"></div>
+                </div>
             </div>
+            <!-- content -->
+
         </div>
-        <!-- content -->
 
     </div>
-
-
 
 
 
