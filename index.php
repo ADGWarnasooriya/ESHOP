4654b1d3b3cc11aa622a24c742b120c0189f8bd3
Dhinid Warnasooriya <phoenixfire696@gmail.com>
2022-10-16 02:45:43 +0530

    signUpBox

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
             <div class="col-12 p-3">
                 <div class="row">
                     <div class="col-6 d-none d-lg-block background"></div>
-                    <div class="col-12 col-lg-6" id="signUpBox">
+                    <div class="col-12 col-lg-6 d-none" id="signUpBox">
                         <div class="row g-2">
                             <div class="col-12">
                                 <p class="title2">Create New Account</p>
@@ -69,8 +69,17 @@
                                     <option value="">Female</option>
                                 </select>
                             </div>
+
+                            <div class="col-12 col-lg-6 d-grid">
+                                <button class="btn btn-primary">Sign Up</button>
+                            </div>
+
+                            <div class="col-12 col-lg-6 d-grid">
+                                <button class="btn btn-dark">Already have an account? Sign In</button>
+                            </div>
                         </div>
                     </div>
+
                 </div>
             </div>
             <!-- content -->
