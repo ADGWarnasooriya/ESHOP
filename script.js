9db333174dd2d52d3e8a4f15521644e8cdca56cb
Dhinid Warnasooriya <phoenixfire696@gmail.com>
2022-10-25 00:46:44 +0530

    checking id's

--- script.js
+++ script.js
@@ -8,5 +8,19 @@ function changeView(){
 }
 
 function signUp(){
-    alert("ok");
+    
+    var f = document.getElementById("f");
+    var l = document.getElementById("l");
+    var e = document.getElementById("e");
+    var p = document.getElementById("p");
+    var m = document.getElementById("m");
+    var g = document.getElementById("g");
+
+    alert(f.value);
+    alert(l.value);
+    alert(e.value);
+    alert(p.value);
+    alert(m.value);
+    alert(g.value);
+    
 }
