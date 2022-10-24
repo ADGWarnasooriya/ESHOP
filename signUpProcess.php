42804cdf88b3b9eb39d7d60f320e1533b0ea9d9d
Dhinid Warnasooriya <phoenixfire696@gmail.com>
2022-10-25 01:09:16 +0530

    form data to php

--- signUpProcess.php
+++ signUpProcess.php
@@ -1,3 +1,20 @@
 <?php
 
-echo("Hello");
+$fname = $_POST["f"];
+$lname = $_POST["l"];
+$email = $_POST["e"];
+$password = $_POST["p"];
+$mobile = $_POST["m"];
+$gender = $_POST["g"];
+
+echo ($fname);
+echo ("<br/>");
+echo ($lname);
+echo ("<br/>");
+echo ($email);
+echo ("<br/>");
+echo ($mobile);
+echo ("<br/>");
+echo ($password);
+echo ("<br/>");
+echo ($gender);
