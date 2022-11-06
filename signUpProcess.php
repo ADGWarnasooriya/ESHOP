2a56fa6c3e3e2a4c6f9c89951d2d76eefa5aa7fd
Dhinid Warnasooriya <phoenixfire696@gmail.com>
2022-11-07 04:22:26 +0530

    success

--- signUpProcess.php
+++ signUpProcess.php
@@ -34,5 +34,22 @@ if (empty($fname)) {
 } else if (!preg_match("/07[0,1,2,3,4,5,6,7,8][0-9]/", $mobile)) {
     echo ("Invalid Mobile !!!");
 } else {
-    echo ("success !");
+
+    $rs = Database::search("SELECT * FROM `user` WHERE `email`='" . $email . "' OR `mobile`='" . $mobile . "'");
+    $n = $rs->num_rows;
+
+    if ($n > 0) {
+        echo ("User with the same Email or Mobile already exists.");
+    } else {
+        $d = new DateTime();
+        $tz = new DateTimeZone("Asia/Colombo");
+        $d->setTimezone($tz);
+        $date = $d->format("Y-m-d H:i:s");
+
+        Database::iud("INSERT INTO `user`
+        (`fname`,`lname`,`email`,`mobile`,`password`,`gender_id`,`joined_date`,`status`) VALUES
+        ('" . $fname . "','" . $lname . "','" . $email . "','" . $mobile . "','" . $password . "','" . $gender . "','" . $date . "','1')");
+
+        echo ("success !");
+    }
 }
